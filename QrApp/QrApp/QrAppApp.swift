30dea6fe3f493f9fe4d571f9e4021f6e53cb1bb0
Furkan Simsek <furkansimek@gmail.com>
2022-09-06 15:49:39 +0300

    First Commit

---
+++ QrApp/QrApp/QrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrAppApp.swift
+//  QrApp
+//
+//  Created by Onur Şimşek on 6.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct QrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
