bcdf47ed1689190a3937a144377b3965b5167fcc
Furkan Simsek <furkansimek@gmail.com>
2022-09-06 17:15:43 +0300

    FirstView

---
+++ QrApp/QrApp/View/FirstView.swift
@@ -0,0 +1,60 @@
+//
+//  FirstView.swift
+//  QrApp
+//
+//  Created by Onur Şimşek on 6.09.2022.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            Image(systemName: "qrcode")
+                .resizable()
+                .frame(width: 40 , height: 40)
+                .foregroundColor(Color.red)
+                .overlay(Circle()
+                    .strokeBorder(lineWidth:0 )
+                    .background(Circle()
+                        .foregroundColor(Color(red: 0.9725490196078431, green: 0.9176470588235294, blue: 0.9058823529411765, opacity: 0.532)))
+                        
+                    .frame(width: 176, height: 185))
+            
+            Spacer()
+            //Pleace give access your Ca...
+            Text("Pleace give access your Camera so that we can \nscan and provide you what is the \ninside the cod")
+                
+                .font(.custom("Inter Medium", size: 13))
+                .foregroundColor(Color(#colorLiteral(red: 0.73, green: 0.73, blue: 0.73, alpha: 1)))
+                .multilineTextAlignment(.center)
+                Spacer()
+//            Button
+                Button {
+                    
+                } label: {
+                    //Rectangle 2
+                    //Let’s Get Started
+                    Text("Let’s Get Started")
+                        .font(.custom("Inter Bold", size: 16)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .background(//Rectangle 2
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color(#colorLiteral(red: 0.9960784316062927, green: 0.4901960790157318, blue: 0.3333333432674408, alpha: 1)))
+                            .frame(width: 319, height: 58))
+                    
+                    
+            }
+            
+            Spacer()
+            
+
+        }
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+    }
+}
---
+++ QrApp/QrApp/View/ScanView.swift
@@ -0,0 +1,20 @@
+//
+//  ScanView.swift
+//  QrApp
+//
+//  Created by Onur Şimşek on 6.09.2022.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScanView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScanView()
+    }
+}
